76089b0d25058335d0d9b7955527cf6561958fa2
Nicole Ospina <nicolosspina2018@gmail.com>
2026-07-01 16:13:46 -0500

    v12.1.0 - API

--- routes/roles.routes.js
+++ routes/roles.routes.js
@@ -14,4 +14,3 @@ router.patch('/:id/estado', authenticateJWT, checkModuleAccess('ROLES'), rolesCo
 router.delete('/:id', authenticateJWT, checkModuleAccess('ROLES'), rolesController.deleteRole);
 
 module.exports = router;
-n
